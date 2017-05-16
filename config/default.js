20ec45892858d49f9048de4f38e5bc7abb34981d
jklepatch <julien@julienklepatch.com>
2017-05-16 16:31:30 +0800

    Corrected formatting on config/default.js

--- config/default.js
+++ config/default.js
@@ -16,9 +16,9 @@ const defaultConfig = {
   db: {
     dialect: 'mysql',
     host: process.env.DB_HOST,
-	  username: process.env.DB_USERNAME,
-	  password: process.env.DB_PASSWORD,
-	  name: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
   },
   session: {
     secure: false,
