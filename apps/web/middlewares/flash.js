4d38b7b40e722bd72223b9843ea92a8c10e3b047
jklepatch <julien@julienklepatch.com>
2017-05-16 16:27:28 +0800

    Corrected formatting on middlewares/flash.js

--- apps/web/middlewares/flash.js
+++ apps/web/middlewares/flash.js
@@ -8,17 +8,17 @@
 const router = require('express').Router();
 
 router.use(function(req, res, next){
-    // if there's a flash message in the session, make it available as a template variable
-    res.locals.flash = req.session.flash;
-    res.flash = function (type, message) {
-      req.session.flash = {
-        type: type,
-        message: message
-      };
+  // if there's a flash message in the session, make it available as a template variable
+  res.locals.flash = req.session.flash;
+  res.flash = function (type, message) {
+    req.session.flash = {
+      type: type,
+      message: message
     };
-    //Delete the flash so that it doesn't appear again in next request
-    delete req.session.flash;
-    next();
+  };
+  //Delete the flash so that it doesn't appear again in next request
+  delete req.session.flash;
+  next();
 });
 
 module.exports = router;
