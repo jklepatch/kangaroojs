413adb034a2b7b6114d47e18e6f8bba6f816a073
jklepatch <julien@julienklepatch.com>
2017-05-16 16:26:32 +0800

    Corrected formatting on User model (2)

--- apps/web/models/user.js
+++ apps/web/models/user.js
@@ -11,57 +11,57 @@ const initModel = function(sequelize, DataTypes) {
       type: DataTypes.STRING, 
       unique:  {
         args: true,
-				msg: 'This email is already used. Please choose another one.'
-			},
-			isEmail: true,
-			allowNull: false
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				min: {
-					args: [8],
-					msg: 'Password must be 8 characters or more.'
-				}
-			},
-			set: function(password) {
-				/*
-				 * If password is too short, we set the field to an arbitrary
-				 * value below 8 characters, to make the validation fail
-				 * If we didn't do this, the hash function would hash the bad password
-				 * into a string more than 8 characters long and validation would
-				 * not fail...
-				 */
-				if(typeof password === 'undefined' || password.length < 8) {
-					return this.setDataValue('password', '0');
-				}
-				this.setDataValue('password', bcrypt.hashSync(password));
-			}
-		},
-		avatar: {
-			type: DataTypes.STRING,
-			allowNull: true,
-			defaultValue: '/img/user.png'
-		},
+        msg: 'This email is already used. Please choose another one.'
+      },
+      isEmail: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: {
+          args: [8],
+          msg: 'Password must be 8 characters or more.'
+        }
+      },
+      set: function(password) {
+      /*
+        * If password is too short, we set the field to an arbitrary
+        * value below 8 characters, to make the validation fail
+        * If we didn't do this, the hash function would hash the bad password
+        * into a string more than 8 characters long and validation would
+        * not fail...
+        */
+        if(typeof password === 'undefined' || password.length < 8) {
+          return this.setDataValue('password', '0');
+        }
+        this.setDataValue('password', bcrypt.hashSync(password));
+      }
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: '/img/user.png'
+    },
     role: {
-			type: DataTypes.ENUM('member', 'admin'),
-			allowNull: false,
-			/*
-			 * There is a mysql quirk that causes it to not throw an error if we try to save a 
-			 * record where an ENUM field has a value not in ENUM. Instead mysql
-			 * will save the record with a blank value for this field. In order to prevent this,
-			 * We need to throw the error manually in the model before it hits mysql
-			 */ 
-			validate: {
-				mustBeInEnum: function (val) {
-					if(['member', 'admin'].indexOf(val) === -1) throw new Error('Role must be either member or admin.');
-				}
-			}
-		}}, {
-			instanceMethods: instanceMethods,
-			classMethods: classMethods
-		});
+      type: DataTypes.ENUM('member', 'admin'),
+      allowNull: false,
+      /*
+        * There is a mysql quirk that causes it to not throw an error if we try to save a 
+        * record where an ENUM field has a value not in ENUM. Instead mysql
+        * will save the record with a blank value for this field. In order to prevent this,
+        * We need to throw the error manually in the model before it hits mysql
+        */ 
+      validate: {
+        mustBeInEnum: function (val) {
+          if(['member', 'admin'].indexOf(val) === -1) throw new Error('Role must be either member or admin.');
+        }
+      }
+    }}, {
+      instanceMethods: instanceMethods,
+      classMethods: classMethods
+    });
 	
   return User;
 };
